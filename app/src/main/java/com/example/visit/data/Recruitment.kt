621589c1b1692963d3dda55061697df22fd7a98a
Takayuki <kingdom815@gmail.com>
2024-11-24 08:49:50 +0900

    uiを先に開発したいので画面を作成するために必要なデータクラスを作成します。 募集一覧を表示するためのデータクラスを用意します。

---
+++ app/src/main/java/com/example/visit/data/Recruitment.kt
@@ -0,0 +1,13 @@
+package com.example.visit.data
+
+/*
+* 企業の募集情報
+ */
+data class Recruitment (
+    //募集タイトル
+    val title: String,
+    //企業名
+    val companyName: String,
+    //募集のサムネ
+    val companyImage: String,
+)

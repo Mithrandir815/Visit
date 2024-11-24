da12436f5ecac7fc8cd747efeca513cb51e2444b
Takayuki <kingdom815@gmail.com>
2024-11-25 05:50:09 +0900

    求人詳細を表示するためのコンポーネントを作成します。

---
+++ app/src/main/java/com/example/visit/ui/recruitmentdetail/compose/atoms/RecruitmentDetail.kt
@@ -0,0 +1,21 @@
+package com.example.visit.ui.recruitmentdetail.compose.atoms
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.visit.data.RecruitmentDetail
+
+@Composable
+fun RecruitmentDetail(
+    recruitmentDetail: RecruitmentDetail
+) {
+
+}
+
+@Preview
+@Composable
+fun PreviewRecruitmentDetail() {
+    val recruitmentDetail = RecruitmentDetail()
+    RecruitmentDetail(
+
+    )
+}

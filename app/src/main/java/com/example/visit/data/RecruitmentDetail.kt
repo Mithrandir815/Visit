678a1f4b82bf7b67b423cfeee02cddd15b57f410
Takayuki <kingdom815@gmail.com>
2024-11-25 06:08:05 +0900

    求人詳細画面を追加するいくつかのパラメータの修正を行う

--- app/src/main/java/com/example/visit/data/RecruitmentDetail.kt
+++ app/src/main/java/com/example/visit/data/RecruitmentDetail.kt
@@ -4,13 +4,16 @@ data class RecruitmentDetail(
     //募集タイトル
     val title: String = "",
     //企業名
-    val companyName: String = "",
-    //募集のサムネ
-    val companyImage: String = "",
+    val company: Company = Company(),
     //なにをやっているのか
     val whatDescription: String = "",
     //なぜやるのか
     val whyDescription: String = "",
     //どうやっているのか
     val howDescription: String = "",
-)
+) {
+    data class Company(
+        val name: String = "",
+        val image: String = "",
+    )
+}
--- app/src/main/java/com/example/visit/ui/recruitmentdetail/compose/atoms/RecruitmentDetails.kt
+++ app/src/main/java/com/example/visit/ui/recruitmentdetail/compose/atoms/RecruitmentDetails.kt
@@ -1,85 +1,93 @@
-package com.example.visit.ui.recruitment.compose.atoms
+package com.example.visit.ui.recruitmentdetail.compose.atoms
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.visit.R
-import com.example.visit.data.Recruitment
+import com.example.visit.data.RecruitmentDetail
 import com.example.visit.ui.theme.SecondaryColor
 
 @Composable
-fun RecruitmentContent(
-    modifier: Modifier = Modifier,
-    recruitment: Recruitment,
-    onClick: (Int) -> Unit,
+fun RecruitmentDetails(
+    modifier: Modifier,
+    recruitmentDetail: RecruitmentDetail,
 ) {
-    Row(
-        modifier = modifier
+    Column(
+        modifier
             .fillMaxWidth()
-            .height(60.dp)
             .background(color = SecondaryColor, shape = RoundedCornerShape(8.dp))
-            .clickable(onClick = { onClick(recruitment.id) })
     ) {
         AsyncImage(
             model = ImageRequest.Builder(context = LocalContext.current)
-                .data(recruitment.company.image)
+                .data(recruitmentDetail.company.image)
                 .crossfade(true)
                 .build(),
             error = painterResource(R.drawable.ic_launcher_foreground),
             placeholder = painterResource(R.drawable.ic_launcher_foreground),
             contentDescription = stringResource(R.string.recruitment_list),
             contentScale = ContentScale.Crop,
-            modifier = Modifier.width(80.dp)
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
         )
-        Column {
-            Text(
-                modifier = Modifier.padding(start = 8.dp, end = 8.dp, top = 4.dp),
-                text = recruitment.title,
-                fontSize = 12.sp,
-                overflow = TextOverflow.Ellipsis,
-                maxLines = 1
-            )
-            Text(
-                text = recruitment.company.name,
-                modifier = Modifier.padding(start = 8.dp, end = 8.dp, bottom = 4.dp),
-                fontSize = 10.sp,
-                overflow = TextOverflow.Ellipsis,
-                maxLines = 1
-            )
-        }
+        Text(
+            modifier = Modifier.padding(4.dp),
+            text = recruitmentDetail.title,
+            fontSize = 24.sp,
+        )
+        Text(
+            modifier = Modifier.padding(4.dp),
+            text = recruitmentDetail.company.name,
+            fontSize = 20.sp,
+        )
+        Text(
+            modifier = Modifier.padding(4.dp),
+            text = recruitmentDetail.whatDescription,
+            fontSize = 16.sp,
+        )
+        Text(
+            modifier = Modifier.padding(4.dp),
+            text = recruitmentDetail.whyDescription,
+            fontSize = 16.sp,
+        )
+        Text(
+            modifier = Modifier.padding(4.dp),
+            text = recruitmentDetail.howDescription,
+            fontSize = 16.sp,
+        )
     }
 }
 
 @Preview
 @Composable
-fun PreviewRecruitmentContent() {
-    val recruitment = Recruitment(
-        id = 1,
+fun PreviewRecruitmentDetails() {
+    val recruitmentDetail = RecruitmentDetail(
         title = "Androidエンジニア",
-        company = Recruitment.Company(
+        company = RecruitmentDetail.Company(
             name = "Hoge株式会社",
             image = "aaaa",
-        )
+        ),
+        whyDescription = "aaaaafjaidhfioajiodhpadhoj",
+        whatDescription = "aaaaafjaidhfioajiodhpadhoj",
+        howDescription = "aaaaafjaidhfioajiodhpadhoj"
     )
-    RecruitmentContent(modifier = Modifier.background(color = Color.White), recruitment, {})
+    RecruitmentDetails(
+        modifier = Modifier,
+        recruitmentDetail = recruitmentDetail,
+    )
 }
